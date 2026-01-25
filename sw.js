82b99ee3515501d0b568a356ae71ede034a4f82c
karthik <mayarakarthik299@gmail.com>
2026-01-25 19:14:43 +0530

    added service worker.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Increment this version string to force an update on user devices
-const APP_VERSION = 'v2.0.5';
+const APP_VERSION = 'v2.0.6';
 const CACHE_NAME = `pdf-toolkit-${APP_VERSION}`;
 
 // ONLY cache local files during install. 
